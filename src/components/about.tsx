33acb38e5775b5dcb379762df032174f5585d0e8
gorakshnath7 <140836805+gorakshnath7@users.noreply.github.com>
2024-02-29 17:57:34 +0530

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -54,7 +54,7 @@ export const About = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="empty-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          I'm a skilled website developer with experience in TypeScript and
+          I'm a skilled website developer & SEO expert with experience in TypeScript and
           Javascript, and expertise in frameworks like React, Node.js, and
           Three.js. I'm a quick learner and collaborate closely with clients to
           create efficient, scalable, and user-friendly solutions that solve
